80548f9a086ec7ab836915aced45e9fe5c717b19
Александр <mechanika.kap@gmail.com>
2023-06-07 08:38:26 +0300

    ДЗ 5 циклы

--- src/Homework5Cycles2.java
+++ src/Homework5Cycles2.java
@@ -7,18 +7,18 @@ public class Homework5Cycles2 {
         int total = 0;
         int month = 0;
         while (total <= 2_459_000) {
-            total = total + total / 100;
             total = total + attachments;
+            month++;
             System.out.println("Месяц " + month + " , сумма накоплений равна" + total + " рублей");
         }
     }
 
     private static void task2() {
-        int Numbers = 0;
-        while (Numbers <= 10) {
-            System.out.print(Numbers);
+        int numbers = 0;
+        while (numbers <= 10) {
+            System.out.print(numbers);
         }
-        for (int i = 10; i <= 0; i--) {
+        for (int i = 10; i >= 0; i--) {
             System.out.println(i);
 
         }
@@ -26,12 +26,12 @@ public class Homework5Cycles2 {
 
     private static void task3() {
         int populationY = 12_000_000;
-        int fertility = (populationY / 1000) * 17;
-        int mortality = (populationY / 1000) * 8;
         int years = 0;
         while (years <= 10) {
-            populationY = populationY + fertility;
-            fertility = fertility - mortality;
+            int fertility = (populationY / 1000) * 17;
+            int mortality = (populationY / 1000) * 8;
+            populationY = populationY + fertility - mortality;
+            years++;
             System.out.println("Год " + years + " , счисленность населения составляет" + populationY + " человек");
         }
     }
@@ -41,7 +41,7 @@ public class Homework5Cycles2 {
         double annualInterest = 0.07;
         double capitalNew = 0;
         int Month = 0;
-        while (capitalNew == 12_000_000) {
+        while (capitalNew <= 12_000_000) {
             capitalNew = capitalInitial + capitalInitial * annualInterest;
             System.out.println("Месяц " + Month + " , новый капитал" + capitalNew + " руб.");
         }
@@ -52,9 +52,9 @@ public class Homework5Cycles2 {
         double annualInterest = 0.07;
         double capitalNew = 0;
         int month = 0;
-        for (; capitalNew == 12_000_000; month++) ;
-        if (month % 2 == 0) {
-            capitalNew = capitalInitial + capitalInitial * annualInterest;
+        for (; capitalNew <= 12_000_000; month++) ;
+        capitalNew = capitalNew + capitalInitial * annualInterest;
+        if (month % 6 == 0) {
             System.out.println("Месяц " + month + " , новый капитал" + capitalNew + " руб.");
         }
     }
@@ -63,35 +63,38 @@ public class Homework5Cycles2 {
         int capitalInitial = 15_000;
         double annualInterest = 0.07;
         double capitalNew = 0;
-        int Month = 0;
-        while (Month == Month * 6) {
+        int year = 9;
+        int month = 0;
+        while (month <= year * 12) {
             capitalNew = capitalInitial + capitalInitial * annualInterest;
-            System.out.println("Месяц6 " + Month + " , новый капитал6" + capitalNew + " руб.");
+            month++;
+            System.out.println("Месяц 6й " + month + " , новый капитал6" + capitalNew + " руб.");
         }
     }
 
     private static void task7() {
         int fridayReport = 5;
         int numbers = 0;
-        while (numbers <= 31) {
-            fridayReport = fridayReport + 7;
-            if (numbers % 5 == 0) {
+        while (fridayReport <= 31) {
                 System.out.println("Сегодня пятница " + fridayReport + "-е число. Необходимо подготовить отчет" + numbers);
+            fridayReport = fridayReport + 7;
             }
-        }
     }
 
     private static void task8() {
         int cometFlies = 79;
-        int years = 0;
-        for (; years <= 2123; years++) {
-            years = years + 79;
-            years = cometFlies + years;
-            if (years >= 1823 && years <= 2123) {
+        for (int years = 0; years <= 2123; years += cometFlies) {
+            if (years >= 1823) {
                 System.out.println(years);
             }
         }
     }
+    private static void task9() {
+        for (int i = 0; i < 12; i++) {
+            System.out.println(i);
+        }
+    }
 }
+
 
 
