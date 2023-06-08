2c754322780acc616eb08715ce293f5e7b274493
Александр <mechanika.kap@gmail.com>
2023-06-08 10:13:11 +0300

    ДЗ 5 циклы

--- src/Homework4Cycles.java
+++ src/Homework4Cycles.java
@@ -1,5 +1,15 @@
 public class Homework4Cycles {
     public static void main(String[] args) {
+        task1();
+        task2();
+        task3();
+        task4();
+        task5();
+        task6();
+        task7();
+        task8();
+        task9();
+        task10();
     }
 
     private static void task1() {
