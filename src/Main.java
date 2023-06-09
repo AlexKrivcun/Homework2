1b5e4624ad5b65509477d1614872bb62aaa31b5a
Александр <mechanika.kap@gmail.com>
2023-06-09 08:51:47 +0300

    ДЗ 5 циклы

--- src/Main.java
+++ src/Main.java
@@ -6,14 +6,14 @@ public class Main {
 
     public static void task1() {
         var staff = (640 / 8);
-        System.out.println("Всего сотрудиков в компании" + staff + "человек");
+        System.out.println("Всего сотрудиков в компании " + staff + " человек");
     }
 
     public static void task2() {
         var staff = (640 / 8);
         staff = staff + 94;
         var total = (640 / staff);
-        System.out.println("Если в компании работает" + staff + "человек");
-        System.out.println("Всего" + total + "часов работы может быть поделено между сотрудниками");
+        System.out.println("Если в компании работает " + staff + " человек");
+        System.out.println("Всего " + total + " часов работы может быть поделено между сотрудниками");
     }
 }
